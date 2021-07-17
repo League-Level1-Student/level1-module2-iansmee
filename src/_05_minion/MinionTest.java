c8ab909bee3aca8094cfc2a4b9a43915368b2e8b
iansmee <53161827+iansmee@users.noreply.github.com>
2021-07-17 14:25:57 -0700

    Finished minion

--- src/_05_minion/MinionTest.java
+++ src/_05_minion/MinionTest.java
@@ -20,6 +20,8 @@ This is because there is no Minion class in the package.
 2.
  So to make these tests pass, you will first need to create a Minion class with the member variables below:
 
+
+
   			private String name; 
   			private int eyes; 
   			private String color; 
---
+++ src/_06_tea_party/TeaParty.java
@@ -0,0 +1,22 @@
+package _06_tea_party;
+
+import javax.swing.JOptionPane;
+
+public class TeaParty {
+	 public String welcome (String name, boolean isWoman, boolean isKnighted) {
+	        
+	 }   
+	 
+	 String name JOptionPane.showInputDialog("what is your name");
+	 
+	 if(isWoman && isKnighted) {
+		 JOptionPane.showMessageDialog("Lady" + name);
+	 
+	 }
+	 }
+	 
+	 
+	 
+	
+
+
